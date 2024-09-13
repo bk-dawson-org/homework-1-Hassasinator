939df574c2318162d658f2072862f0a8ccd26eb5
Hassasinator <zahid.hassan@dawsoncollege.qc.ca>
2024-09-13 22:38:40 +0000

    Main

---
+++ main.py
@@ -0,0 +1,9 @@
+#calculating volume of a sphere
+#formula of sphere = (4/3)*pi*r^3
+import math
+
+
+r = int(input("enter radius of the circle: "))
+
+vol_sph = (4/3)*(math.pi)*(r**3)
+print("the volume of the sphere is: ", vol_sph)
